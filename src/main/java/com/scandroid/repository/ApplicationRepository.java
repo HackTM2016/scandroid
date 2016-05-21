ac0bab1bb9061b3287f519d2d7b0147d4438ffd3
Simioana Mihai <mihaisimi@yahoo.com>
2016-05-21 23:21:53 +0300

    Find apps that need scanning.

--- src/main/java/com/scandroid/repository/ApplicationRepository.java
+++ src/main/java/com/scandroid/repository/ApplicationRepository.java
@@ -15,4 +15,7 @@ public interface ApplicationRepository extends JpaRepository<Application,Long> {
     //@Query("SELECT DISTINCT  from Link l LEFT JOIN l.scan s JOIN FETCH s.application a WHERE a.packageName= (:packageName)")
     @Query("SELECT DISTINCT a from Application a LEFT JOIN FETCH a.scans s LEFT JOIN FETCH s.links l WHERE a.packageName= (:packageName)")
     List<Application> findAllByApplicationName(@Param("packageName") String packageName);
+
+    @Query("SELECT DISTINCT a from Application a LEFT JOIN FETCH a.scans s")
+    List<Application> findAllThatNeedScanning();
 }
--- src/main/java/com/scandroid/service/ScheduledApkTester.java
+++ src/main/java/com/scandroid/service/ScheduledApkTester.java
@@ -3,24 +3,45 @@ package com.scandroid.service;
 /**
  * Created by mihai on 21/05/16.
  */
-    import org.slf4j.Logger;
-    import org.slf4j.LoggerFactory;
-    import org.springframework.scheduling.annotation.Scheduled;
+
+import com.scandroid.domain.Application;
+import com.scandroid.domain.Scan;
+import com.scandroid.repository.ApplicationRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.inject.Inject;
 import java.text.SimpleDateFormat;
-import java.util.Date;
 
 @Component
 public class ScheduledApkTester {
 
     private final Logger log = LoggerFactory.getLogger(ScheduledApkTester.class);
 
+    @Inject
+    private ApplicationRepository applicationRepository;
 
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
-    @Scheduled(fixedRate = 5000)
-    public void reportCurrentTime() {
-        log.error("The time is now " + dateFormat.format(new Date()));
+    @Scheduled(fixedRate = 10000)
+    public void appsThatNeedScanning() {
+        log.debug("Apps that need scanning:");
+        for(Application a: applicationRepository.findAllThatNeedScanning()){
+            boolean scan = true;
+            if(!a.getScans().isEmpty()){
+                for(Scan s: a.getScans()){
+                    if(s.isSuccess()){
+                        scan = false;
+                        break;
+                    }
+                }
+            }
+            if(scan){
+                log.debug("Scanning:"+a.getPackageName());
+            }
+        }
+
     }
 }
